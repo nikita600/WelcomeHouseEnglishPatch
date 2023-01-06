ff9593101daf3ce375e92855136efe7625b80dd8
nikita600 <nikitak600@gmail.com>
2023-01-06 20:23:03 +0300

    Split text files by groups.

--- apply_patch.bat
+++ apply_patch.bat
@@ -1,2 +1,5 @@
-armips.exe main.asm -temp result.asm
+bin\armips.exe main.asm -temp result.asm
+bin\m3checksum.exe exe\src_PSX.EXE
+bin\m3checksum.exe exe\PSX.EXE
+
 pause
---
+++ src/text_05.asm
@@ -0,0 +1,15 @@
+.sjis (LANG == "jap") ? \
+	"１階" : \
+	"1F"
+
+.sjis (LANG == "jap") ? \
+	"２階" : \
+	"2F"
+
+.sjis (LANG == "jap") ? \
+	"３階" : \
+	"3F"
+
+.sjis (LANG == "jap") ? \
+	"地下" : \
+	"BS"
