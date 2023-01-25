6924698931a2d3a5e0b30e6ac85f7d232df3dabd
nikita600 <nikitak600@gmail.com>
2023-01-25 23:08:35 +0300

    Render fonts research.

--- apply_patch.bat
+++ apply_patch.bat
@@ -1,5 +1,4 @@
 bin\armips.exe main.asm -temp result.asm
 bin\m3checksum.exe exe\src_PSX.EXE
 bin\m3checksum.exe exe\PSX.EXE
-
-pause
+::pause
--- src/render_text.asm
+++ src/render_text.asm
@@ -1,154 +1,202 @@
 ;--------------------------------------
 
+CHAR_8x16:
+
+@char_height equ 0x0F
+
+@dst_buff equ t0
+@src_buff equ t1
+@line_idx equ t2
+
+;--------------------------------------
+
+.org 0x80030af4
+.ifndef CHAR_8x16
+    sll v0,s0,0x2
+.else
+    sll v0,s0,0x1
+.endif
+
+
 .org 0x80030c28
 .area 0x80030e70 - 0x80030c28
+
+;a0 = char_code
+;a1, s0 = char_buff
 
 CopyCharTexToBuf_80030c28:
     addiu   sp,sp,-0x18
     sw      ra,0x14(sp)
     sw      s0,0x10(sp)
+
     jal     GetBiosCharTexturePtr_80030e70
     move    s0,a1
-    move    t1,v0
+
+    move    @src_buff,v0
     li      v0,-0x1
-    bne     t1,v0,convert_to_16bpp_80030c54
-    move    t0,s0
+    bne     @src_buff,v0,convert_to_16bpp_80030c54
+    move    @dst_buff,s0
+
     j       return_80030e5c
     li      v0,0x1
 
 convert_to_16bpp_80030c54:
-    move    $t2, $zero
+    move    @line_idx, zero
 
 loop_80030c58:
-    srl        a0,t2,0x1f
-    addu       a0,t2,a0
+    srl        a0,@line_idx,0x1f
+    addu       a0,@line_idx,a0
     sra        a0,a0,0x1
     addiu      a0,a0,0x1
-    lbu        v1,0x0(t1)
+
+@entry_01: ; 7-6-5-4 (0-4-8-12)
+    lbu        v1,0x0(@src_buff)
     nop
-    srl        v1,v1,0x7
+    srl        v1,v1,7
     mult       v1,a0
     mflo       v1
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0x6
+    srl        v0,v0,6
     andi       v0,v0,0x1
-    sll        a3,a0,0x4
+    sll        a3,a0,4
     mult       v0,a3
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0x5
+    srl        v0,v0,5
     andi       v0,v0,0x1
-    sll        a2,a0,0x8
+    sll        a2,a0,8
     mult       v0,a2
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0x4
+    srl        v0,v0,4
     andi       v0,v0,0x1
-    sll        a1,a0,0xc
+    sll        a1,a0,12
     mult       v0,a1
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    addiu      t0,t0,0x2
-    lhu        v1,0x0(t1)
+    sh         v1,0x0(@dst_buff)
+
+    addiu      @dst_buff,@dst_buff,0x2
+    
+@entry_02: ; 3-2-1-0 (0-0-0-0)
+    lhu        v1,0x0(@src_buff)
     nop
-    srl        v1,v1,0x3
+    srl        v1,v1,3
     andi       v1,v1,0x1
     mult       v1,a0
     mflo       v1
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0x2
+    srl        v0,v0,2
     andi       v0,v0,0x1
     mult       v0,a3
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0x1
+    srl        v0,v0,1
     andi       v0,v0,0x1
     mult       v0,a2
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    andi       v0,v0,0x1
+    andi       v0,v0,1
     mult       v0,a1
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    addiu      t0,t0,0x2
-    lhu        v1,0x0(t1)
+    sh         v1,0x0(@dst_buff)
+
+    addiu      @dst_buff,@dst_buff,0x2
+
+@entry_03: ; 15-14-13-12 (0-0-0-0)
+    lhu        v1,0x0(@src_buff)
     nop
-    srl        v1,v1,0xf
+    srl        v1,v1,15
     mult       v1,a0
     mflo       v1
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    ;sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0xe
-    andi       v0,v0,0x1
+    srl        v0,v0,14
+    andi       v0,v0,1
     mult       v0,a3
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    ;sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0xd
-    andi       v0,v0,0x1
+    srl        v0,v0,13
+    andi       v0,v0,1
     mult       v0,a2
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    ;sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0xc
-    andi       v0,v0,0x1
+    srl        v0,v0,12
+    andi       v0,v0,1
     mult       v0,a1
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    addiu      t0,t0,0x2
-    lhu        v1,0x0(t1)
+    ;sh         v1,0x0(@dst_buff)
+
+    addiu      @dst_buff,@dst_buff,0x2
+
+@entry_04: ; 11-10-9
+    lhu        v1,0x0(@src_buff)
     nop
-    srl        v1,v1,0xb
+    srl        v1,v1,11
     andi       v1,v1,0x1
     mult       v1,a0
     mflo       v1
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    ;sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0xa
-    andi       v0,v0,0x1
+    srl        v0,v0,10
+    andi       v0,v0,1
     mult       v0,a3
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    lhu        v0,0x0(t1)
+    ;sh         v1,0x0(@dst_buff)
+
+    lhu        v0,0x0(@src_buff)
     nop
-    srl        v0,v0,0x9
-    andi       v0,v0,0x1
+    srl        v0,v0,9
+    andi       v0,v0,1
     mult       v0,a2
     mflo       v0
     addu       v1,v1,v0
-    sh         v1,0x0(t0)
-    addiu      t0,t0,0x2
-    addiu      t2,t2,0x1
-        
-    slti       v0,t2,0xf
+    ;sh         v1,0x0(@dst_buff)
+
+    addiu      @dst_buff,@dst_buff,0x2
+
+@next_line:
+    addiu      @line_idx,@line_idx,0x1 
+    slti       v0,@line_idx,@char_height
     bne        v0,0,loop_80030c58
-    addiu      t1,t1,0x2
+    addiu      @src_buff,@src_buff,0x2
     clear      v0
 
 return_80030e5c:
@@ -170,12 +218,19 @@ Krom2RawAdd equ  0x8004e5d4
 GetBiosCharTexturePtr_80030e70:
     addiu      sp,sp,-0x18
     sw         ra,0x10(sp)
+
     lbu        v0,0x0(a0)
     nop
     sll        v0,v0,0x8
     lbu        a0,0x1(a0)
+
+.ifndef CHAR_8x16
     jal        Krom2RawAdd
     or        a0,v0
+.else
+    li v0, 0xBFC7F90A
+.endif
+
     lw         ra,0x10(sp)
     addiu      sp,sp,0x18
     jr         ra
