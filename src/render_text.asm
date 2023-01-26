a493e1df540b4b0f96e970faa78415757e5bc706
nikita600 <nikitak600@gmail.com>
2023-01-26 20:25:19 +0300

    Base for text rendering ASCII.

--- src/render_text.asm
+++ src/render_text.asm
@@ -1,25 +1,38 @@
 ;--------------------------------------
 
-CHAR_8x16:
+;USE_SHIFT_JIS:
 
 @char_height equ 0x0F
-
-@dst_buff equ t0
-@src_buff equ t1
-@line_idx equ t2
 
 ;--------------------------------------
 
 .org 0x80030af4
-.ifndef CHAR_8x16
+.ifdef USE_SHIFT_JIS
     sll v0,s0,0x2
 .else
     sll v0,s0,0x1
 .endif
 
+.org 0x80030b0c
+.ifdef USE_SHIFT_JIS
+    addiu   s2,s2,0x2
+.else
+    addiu   s2,s2,0x1
+.endif
+
+;--------------------------------------
 
 .org 0x80030c28
 .area 0x80030e70 - 0x80030c28
+
+@mask0001   equ a0
+@mask1000   equ a1
+@mask0100   equ a2
+@mask0010   equ a3
+
+@dst_buff equ t0
+@src_buff equ t1
+@line_idx equ t2
 
 ;a0 = char_code
 ;a1, s0 = char_buff
@@ -44,45 +57,53 @@ convert_to_16bpp_80030c54:
     move    @line_idx, zero
 
 loop_80030c58:
-    srl        a0,@line_idx,0x1f
-    addu       a0,@line_idx,a0
-    sra        a0,a0,0x1
-    addiu      a0,a0,0x1
+    srl        @mask0001,@line_idx,31 ; (((line_idx >> 0x1F) + line_idx) >> 1) + 1
+    addu       @mask0001,@line_idx,@mask0001
+    sra        @mask0001,@mask0001,0x1
+    addiu      @mask0001,@mask0001,0x1
+
+.ifdef USE_SHIFT_JIS
+
+;--------------------------------------
 
 @entry_01: ; 7-6-5-4 (0-4-8-12)
-    lbu        v1,0x0(@src_buff)
+; @dst_buff = (@src_buff > 7) * @mask0001
+    lbu        v1,0x0(@src_buff) 
     nop
     srl        v1,v1,7
-    mult       v1,a0
+    mult       v1,@mask0001
     mflo       v1
     sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff > 6) & 1) * @mask0010
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,6
     andi       v0,v0,0x1
-    sll        a3,a0,4
-    mult       v0,a3
+    sll        @mask0010,@mask0001,4
+    mult       v0,@mask0010
     mflo       v0
     addu       v1,v1,v0
     sh         v1,0x0(@dst_buff)
 
+ ; @dst_buff = ((@src_buff > 5) & 1) * @mask0100
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,5
     andi       v0,v0,0x1
-    sll        a2,a0,8
-    mult       v0,a2
+    sll        @mask0100,@mask0001,8
+    mult       v0,@mask0100
     mflo       v0
     addu       v1,v1,v0
     sh         v1,0x0(@dst_buff)
 
-    lhu        v0,0x0(@src_buff)
+ ; @dst_buff = ((@src_buff > 4) & 1) * @mask1000
+    lhu        v0,0x0(@src_buff) 
     nop
     srl        v0,v0,4
     andi       v0,v0,0x1
-    sll        a1,a0,12
-    mult       v0,a1
+    sll        @mask1000,@mask0001,12
+    mult       v0,@mask1000
     mflo       v0
     addu       v1,v1,v0
     sh         v1,0x0(@dst_buff)
@@ -90,36 +111,40 @@ loop_80030c58:
     addiu      @dst_buff,@dst_buff,0x2
     
 @entry_02: ; 3-2-1-0 (0-0-0-0)
+; @dst_buff = ((@src_buff >> 3) & 1) * @mask0001
     lhu        v1,0x0(@src_buff)
     nop
     srl        v1,v1,3
     andi       v1,v1,0x1
-    mult       v1,a0
+    mult       v1,@mask0001
     mflo       v1
     sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff >> 2) & 1) * @mask0010
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,2
     andi       v0,v0,0x1
-    mult       v0,a3
+    mult       v0,@mask0010
     mflo       v0
     addu       v1,v1,v0
     sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff >> 1) & 1) * @mask0100
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,1
     andi       v0,v0,0x1
-    mult       v0,a2
+    mult       v0,@mask0100
     mflo       v0
     addu       v1,v1,v0
     sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff >> 0) & 1) * @mask1000
     lhu        v0,0x0(@src_buff)
     nop
     andi       v0,v0,1
-    mult       v0,a1
+    mult       v0,@mask1000
     mflo       v0
     addu       v1,v1,v0
     sh         v1,0x0(@dst_buff)
@@ -127,76 +152,184 @@ loop_80030c58:
     addiu      @dst_buff,@dst_buff,0x2
 
 @entry_03: ; 15-14-13-12 (0-0-0-0)
+; @dst_buff = (@src_buff >> 15) * @mask0001
     lhu        v1,0x0(@src_buff)
     nop
     srl        v1,v1,15
-    mult       v1,a0
+    mult       v1,@mask0001
     mflo       v1
-    ;sh         v1,0x0(@dst_buff)
+    sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff >> 14) & 1) * @mask0010
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,14
-    andi       v0,v0,1
-    mult       v0,a3
+    andi       v0,v0,0x1
+    mult       v0,@mask0010
     mflo       v0
     addu       v1,v1,v0
-    ;sh         v1,0x0(@dst_buff)
+    sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff >> 13) & 1) * @mask0100
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,13
-    andi       v0,v0,1
-    mult       v0,a2
+    andi       v0,v0,0x1
+    mult       v0,@mask0100
     mflo       v0
     addu       v1,v1,v0
-    ;sh         v1,0x0(@dst_buff)
+    sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff >> 12) & 1) * @mask1000
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,12
-    andi       v0,v0,1
-    mult       v0,a1
+    andi       v0,v0,0x1
+    mult       v0,@mask1000
     mflo       v0
     addu       v1,v1,v0
-    ;sh         v1,0x0(@dst_buff)
+    sh         v1,0x0(@dst_buff)
 
     addiu      @dst_buff,@dst_buff,0x2
 
 @entry_04: ; 11-10-9
+; @dst_buff = ((@src_buff >> 11) & 1) * @mask1000
     lhu        v1,0x0(@src_buff)
     nop
     srl        v1,v1,11
     andi       v1,v1,0x1
-    mult       v1,a0
+    mult       v1,@mask0001
     mflo       v1
-    ;sh         v1,0x0(@dst_buff)
+    sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff >> 10) & 1) * @mask1000
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,10
-    andi       v0,v0,1
-    mult       v0,a3
+    andi       v0,v0,0x1
+    mult       v0,@mask0010
     mflo       v0
     addu       v1,v1,v0
-    ;sh         v1,0x0(@dst_buff)
+    sh         v1,0x0(@dst_buff)
 
+; @dst_buff = ((@src_buff >> 9) & 1) * @mask1000
     lhu        v0,0x0(@src_buff)
     nop
     srl        v0,v0,9
     andi       v0,v0,1
-    mult       v0,a2
+    mult       v0,@mask0100
     mflo       v0
     addu       v1,v1,v0
-    ;sh         v1,0x0(@dst_buff)
+    sh         v1,0x0(@dst_buff)
 
     addiu      @dst_buff,@dst_buff,0x2
+
+;--------------------------------------
+
+.else
+
+;--------------------------------------
+
+@entry_01: ; 7-6-5-4 (0-4-8-12)
+; @dst_buff = (@src_buff > 7) * @mask0001
+    lbu        v1,0x0(@src_buff) 
+    nop
+    srl        v1,v1,7
+    mult       v1,@mask0001
+    mflo       v1
+    sh         v1,0x0(@dst_buff)
+
+; @dst_buff = ((@src_buff > 6) & 1) * @mask0010
+    lbu        v0,0x0(@src_buff)
+    nop
+    srl        v0,v0,6
+    andi       v0,v0,0x1
+    sll        @mask0010,@mask0001,4
+    mult       v0,@mask0010
+    mflo       v0
+    addu       v1,v1,v0
+    sh         v1,0x0(@dst_buff)
+
+ ; @dst_buff = ((@src_buff > 5) & 1) * @mask0100
+    lbu        v0,0x0(@src_buff)
+    nop
+    srl        v0,v0,5
+    andi       v0,v0,0x1
+    sll        @mask0100,@mask0001,8
+    mult       v0,@mask0100
+    mflo       v0
+    addu       v1,v1,v0
+    sh         v1,0x0(@dst_buff)
+
+ ; @dst_buff = ((@src_buff > 4) & 1) * @mask1000
+    lbu        v0,0x0(@src_buff) 
+    nop
+    srl        v0,v0,4
+    andi       v0,v0,0x1
+    sll        @mask1000,@mask0001,12
+    mult       v0,@mask1000
+    mflo       v0
+    addu       v1,v1,v0
+    sh         v1,0x0(@dst_buff)
+
+    addiu      @dst_buff,@dst_buff,0x2
+    
+@entry_02: ; 3-2-1-0 (0-0-0-0)
+; @dst_buff = ((@src_buff >> 3) & 1) * @mask0001
+    lbu        v1,0x0(@src_buff)
+    nop
+    srl        v1,v1,3
+    andi       v1,v1,0x1
+    mult       v1,@mask0001
+    mflo       v1
+    sh         v1,0x0(@dst_buff)
+
+; @dst_buff = ((@src_buff >> 2) & 1) * @mask0010
+    lbu        v0,0x0(@src_buff)
+    nop
+    srl        v0,v0,2
+    andi       v0,v0,0x1
+    mult       v0,@mask0010
+    mflo       v0
+    addu       v1,v1,v0
+    sh         v1,0x0(@dst_buff)
+
+; @dst_buff = ((@src_buff >> 1) & 1) * @mask0100
+    lbu        v0,0x0(@src_buff)
+    nop
+    srl        v0,v0,1
+    andi       v0,v0,0x1
+    mult       v0,@mask0100
+    mflo       v0
+    addu       v1,v1,v0
+    sh         v1,0x0(@dst_buff)
+
+; @dst_buff = ((@src_buff >> 0) & 1) * @mask1000
+    lbu        v0,0x0(@src_buff)
+    nop
+    andi       v0,v0,1
+    mult       v0,@mask1000
+    mflo       v0
+    addu       v1,v1,v0
+    sh         v1,0x0(@dst_buff)
+
+    addiu      @dst_buff,@dst_buff,0x2
+
+;--------------------------------------
+
+.endif
 
 @next_line:
     addiu      @line_idx,@line_idx,0x1 
     slti       v0,@line_idx,@char_height
     bne        v0,0,loop_80030c58
+
+.ifdef USE_SHIFT_JIS
     addiu      @src_buff,@src_buff,0x2
+.else
+    addiu      @src_buff,@src_buff,0x1
+.endif
+
     clear      v0
 
 return_80030e5c:
@@ -224,7 +357,7 @@ GetBiosCharTexturePtr_80030e70:
     sll        v0,v0,0x8
     lbu        a0,0x1(a0)
 
-.ifndef CHAR_8x16
+.ifdef USE_SHIFT_JIS
     jal        Krom2RawAdd
     or        a0,v0
 .else
